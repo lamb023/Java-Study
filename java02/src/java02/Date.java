4d2f8ef470571937632b5872978207871228e988
jin-yang <jin-yang@218.38.137.28>
2022-03-06 00:30:48 +0900

    First commit

---
+++ java02/src/java02/Date.java
@@ -0,0 +1,40 @@
+package java02;
+
+public class Date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char var1 ='h';//한글자의 한글 또는 영문 가능 
+		String var2 = "안드로이드";// 여러글자의 문자열 가능 
+		//문자형 
+		
+		byte var3 = -128; //-128 ~ +127까지 가능 
+		short var4 = -32768; //-32768 ~ +32768까지 가능 
+		int var5 = 9999999; //-21억 ~ +21억까지 가능 
+		long var6 = 9999999; // 여러숫자 입력 가능
+		//정수형 
+		
+		float var7 = 0.5f; // 소수점 선언 후 뒤에 f를 까먹지 말고 붙히자 
+		double var8 = 0.0005; // 소수점 선언 
+		//실수형 
+		
+		boolean isStart = false; // true or false를 입력하여 조건식에서 많이 사용합니다 
+		
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+		
+		System.out.println(var7);
+		System.out.println(var8);
+		
+		System.out.println(isStart);
+
+		
+	}
+
+}
