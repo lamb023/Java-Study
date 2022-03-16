88f4a76722134edf56cb9fd0961d500901aecbc1
jin-yang <jin-yang@218.38.137.28>
2022-03-16 23:56:06 +0900

    Java Study-2

---
+++ School-Java/src/schoolJava/test01.java
@@ -0,0 +1,23 @@
+/*
+ 파일이름 : BlockComment.Java
+ 작성자 : 홍길동
+ 작성일 : 2012년 9월 25일
+ 작성이유 : System.out.println 메소드 기능 텟
+ */
+package schoolJava;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(7);
+		System.out.println(3.15);
+		System.out.println("3+5=" +8);//문자와 숫자 연결하기 위해 +
+		System.out.println(3.15 +"는 실수입니다.");
+		System.out.println("3+5의" + "연산결과입니다");
+		System.out.println(3+5);
+		
+		
+	}
+	
+}
